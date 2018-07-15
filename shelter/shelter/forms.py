636b99fcad74aa308334d247d2d751fb94ea64fe
Luke Callow <lgcallow88@gmail.com>
2018-07-15 21:21:13 +0100

    Contact us form working

--- shelter/shelter/forms.py
+++ shelter/shelter/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 
 class ContactUsForm(forms.Form):
-    name = forms.CharField(label='Your name', max_length= 30)
+    name = forms.CharField(label='Your name',required=True)
     email = forms.EmailField()
-    subject = forms.CharField(max_length= 20,)
+    subject = forms.CharField(required=True)
     text = forms.CharField(widget=forms.Textarea)
-    ccmyself = forms.BooleanField(required=False)
+    cc_myself = forms.BooleanField(required=False)
--- shelter/shelter/views.py
+++ shelter/shelter/views.py
@@ -11,20 +11,27 @@ def about(request):
     return render(request, 'index/about.html')
 
 def contact(request):
+    form_class = ContactUsForm
+
     if request.method == 'POST':
-        form = ContactUsForm()
+        form = form_class(data=request.POST)
         if form.is_valid():
             name = form.cleaned_data['name']
             sender = form.cleaned_data['email']
-            subject = '****EMAIL FROM USER ****' + form.cleaned_data['subject']
+            subject = form.cleaned_data['subject']
             message = form.cleaned_data['text']
-
-            recipients = [Config.EMAIL_HOST_USER]
+            cc_myself = form.cleaned_data['cc_myself']
             if cc_myself:
-                recipients.append(sender)
+                recipients = [sender]
+                send_mail(subject,message,sender,recipients)
+            subject = '***CONTACT US FORM ***' + subject
+            recipients = [Config.EMAIL_HOST_USER]
             send_mail(subject, message, sender, recipients)
 
             return HttpResponseRedirect(reverse('index'))
+        else:
+            form = ContactUsForm
+            return render(request, 'index/contactus.html', {'form':form})
     else:
-        form = ContactUs()
+        form = ContactUsForm
         return render(request, 'index/contactus.html',{'form': form})
